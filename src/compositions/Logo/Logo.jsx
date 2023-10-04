6a2f98c8dd604e6147d3fa542de6630544c3c2f9
Tauqeer180 <tauqeermalik855@gmail.com>
2023-10-04 22:18:01 +0500

    Flag Url Updated

--- src/compositions/Logo/Logo.jsx
+++ src/compositions/Logo/Logo.jsx
@@ -3,14 +3,16 @@ import { ImageContainer } from '../../components';
 import styles from './Logo.module.css';
 import { logo, sportLogoBlack,flag } from '../../assets';
 import { useThemeContext } from '../../utilities/themeQuery';
+import { useSelector } from 'react-redux';
 
 const Logo = ({ name,  href, alt, className = '' }) => {
   const themeVariant = useThemeContext();
+  const flagUrl = useSelector((state) => state?.origin?.flagUrl)
 
   return (
       <Link className={`logo ${styles.logo} ${className}`.trim()} to={href}>
         <ImageContainer
-          src={name=='Logo' ? logo : flag}
+          src={name=='Logo' ? logo : flagUrl}
           containerClass={name === 'Logo' ? 'logo-image' : 'flag-image'}  // Apply class based on name value
           alt={alt}
       />
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Root, ErrorPage, Table, Article, topicLoader, articleLoader ,Highlights  } from './routes';
 import './main.css';
-
+import { Provider } from 'react-redux';
+import store from './redux/store'
 const router = createBrowserRouter([
   {
     path: '/',
@@ -21,6 +22,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
+
     <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
